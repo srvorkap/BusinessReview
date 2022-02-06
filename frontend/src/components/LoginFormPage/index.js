97c315aeb1cc4405e8ad427ec3b401fd0200e4aa
Srdan <srdan.vorkapic@outlook.com>
2022-02-05 20:13:39 -0800

    LoginFormPage

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect, NavLink } from "react-router-dom";
+import "./LoginForm.css";
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  // if logged in redirect to (it used to be '/')
+  if (sessionUser) return <Redirect to="/notes" />;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async res => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  return (
+    <div id="whole-login-page">
+      <div id="login-image">{/* <img src={logo}></img> */}</div>
+      <div id="login-form-page">
+        <form onSubmit={handleSubmit}>
+          <ul className="errors">
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+          <div>
+            <input
+              type="text"
+              value={credential}
+              onChange={e => setCredential(e.target.value)}
+              placeholder="Email address or username"
+              className="signup-login-inputs"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              placeholder="Password"
+              className="signup-login-inputs"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Continue"
+            className="signup-login-inputs signup-login-inputs-submit"
+          />
+          {/* <button type="submit">Log In</button> */}
+        </form>
+        <div id="signup-login">
+          <p>Don't have an account?</p>
+          <NavLink to="/signup" id="signup-login-link">
+            Create account
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginFormPage;
---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import "./ProfileButton.css";
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = e => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+
+      <NavLink to='/notes' className="navbar-links">Notes</NavLink>
+      <button onClick={openMenu}>
+        <div id="icon-profile" >
+          <i className="fas fa-user-circle" />
+        </div>
+      </button>
+
+      {showMenu && (
+
+
+        <ul id="profile-dropdown">
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>
+              <button onClick={logout} id="logout-button">Log Out</button>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default ProfileButton;
