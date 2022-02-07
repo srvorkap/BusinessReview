9adc0d773e92c491adbe43bf1e25ca1aec2550cc
Srdan <srdan.vorkapic@outlook.com>
2022-02-06 18:30:52 -0800

    Post business route

--- backend/routes/api/businesses.js
+++ backend/routes/api/businesses.js
@@ -1,24 +1,50 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler')
-const { requireAuth } = require('../../utils/auth')
-const { check, validationResult } = require('express-validator')
-const { Business } = require('../../db/models')
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const { requireAuth } = require("../../utils/auth");
+const { check, validationResult } = require("express-validator");
+const { Business } = require("../../db/models");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
-    const businesses = await Business.findAll()
-    return res.json(businesses)
-}))
+router.get(
+    "/",
+    asyncHandler(async (req, res) => {
+        const businesses = await Business.findAll();
+        return res.json(businesses);
+    })
+);
 
-// router.get('/', (req, res) => {
-//     re
-// })
+router.post(
+    "/",
+    // requireAuth,
+    asyncHandler(async (req, res) => {
+        const {
+            imageURL,
+            name,
+            description,
+            address,
+            city,
+            state,
+            zipCode,
+            phone,
+            hours,
+            userId,
+        } = req.body;
+        const business = Business.build({
+            imageURL,
+            name,
+            description,
+            address,
+            city,
+            state,
+            zipCode,
+            phone,
+            hours,
+            userId,
+        });
+        await business.save();
+        res.json(business);
+    })
+);
 
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
