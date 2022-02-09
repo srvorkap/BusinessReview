5258500430299ec91f2ed2ddf3c5f654a4e42611
Srdan <srdan.vorkapic@outlook.com>
2022-02-08 21:32:35 -0800

    helmet

--- backend/app.js
+++ backend/app.js
@@ -21,12 +21,16 @@ if (!isProduction) {
     // enable cors only in development
     app.use(cors());
 }
-// helmet helps set a variety of headers to better secure your app
-app.use(
-    helmet({
-        contentSecurityPolicy: false,
-    })
-);
+
+// // helmet helps set a variety of headers to better secure your app
+// app.use(
+//     helmet({
+//         contentSecurityPolicy: false,
+//     })
+// );
+
+// ************** helmet change for images ******************
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 // Set the _csrf token and create req.csrfToken method
 app.use(
