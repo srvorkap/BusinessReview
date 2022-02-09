ba11c0a58090bf478d09dda188765e4382ee6607
Srdan <srdan.vorkapic@outlook.com>
2022-02-09 13:01:31 -0800

    Signup page styling

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -3,97 +3,96 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import "./SignupForm.css";
-// import logo from "../../images/newLogo.png";
+import signupLoginImage from '../../images/signupLogin.png'
 
 function SignupFormPage() {
-  const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/business" />;
+    if (sessionUser) return <Redirect to="/business" />;
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (password === confirmPassword) {
-      setErrors([]);
-      return dispatch(
-        sessionActions.signup({ email, username, password })
-      ).catch(async res => {
-        const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
-      });
-    }
-    return setErrors([
-      "Confirm Password field must be the same as the Password field",
-    ]);
-  };
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+            setErrors([]);
+            return dispatch(
+                sessionActions.signup({ email, username, password })
+            ).catch(async res => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            });
+        }
+        return setErrors([
+            "Confirm Password field must be the same as the Password field",
+        ]);
+    };
 
-  return (
-    <div id="whole-signup-page">
-      <div id="signup-image">{/* <img src={logo}></img> */}</div>
-      <div id="signup-form-page">
-        <form onSubmit={handleSubmit}>
-          <ul className="errors">
-            {errors.map((error, idx) => (
-              <li key={idx}>{error}</li>
-            ))}
-          </ul>
-          <div>
-            <input
-              type="text"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              placeholder="Email"
-              className="signup-login-inputs"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-              placeholder="Username"
-              className="signup-login-inputs"
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              placeholder="Password"
-              className="signup-login-inputs"
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-              placeholder="Confirm Password"
-              className="signup-login-inputs"
-            />
-          </div>
-          <input
-            type="submit"
-            value="Sign Up"
-            className="signup-login-inputs signup-login-inputs-submit"
-          />
-          {/* <button type="submit">Sign Up</button> */}
-        </form>
-        <div id="signup-login">
-          <p>Already on BusinessReview?</p>
-          <NavLink to="/login" id="signup-login-link">
-            Log in
-          </NavLink>
+    return (
+        <div id="whole-signup-page">
+            <div id="signup-form-page">
+                <form onSubmit={handleSubmit}>
+                    <ul className="errors">
+                        {errors.map((error, idx) => (
+                            <li key={idx}>{error}</li>
+                        ))}
+                    </ul>
+                    <div>
+                        <input
+                            type="text"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            placeholder="Email"
+                            className="signup-login-inputs"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            placeholder="Username"
+                            className="signup-login-inputs"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            placeholder="Password"
+                            className="signup-login-inputs"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            value={confirmPassword}
+                            onChange={e => setConfirmPassword(e.target.value)}
+                            placeholder="Confirm Password"
+                            className="signup-login-inputs"
+                        />
+                    </div>
+                    <input
+                        type="submit"
+                        value="Sign Up"
+                        className="signup-login-inputs signup-login-inputs-submit"
+                    />
+                </form>
+                <div id="signup-login">
+                    <p>Already on BusinessReview?</p>
+                    <NavLink to="/login" id="signup-login-link">
+                        Log in
+                    </NavLink>
+                </div>
+            </div>
+            <div id="signup-image"><img src={signupLoginImage}></img>}</div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default SignupFormPage;
