b52f666ecf0d7d352f718a2f1f2ed00fc0859ed2
Srdan <srdan.vorkapic@outlook.com>
2022-02-10 09:58:07 -0800

    Post review route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,9 +7,45 @@ const { Review } = require('../../db/models')
 
 const router = express.Router()
 
+const reviewValidator = [
+    check('rating')
+        .exists({ checkFalsy: true })
+        .withMessage('Please rate this business.'),
+    check('content')
+        .exists({ checkFalsy: true })
+        .withMessage('Please write a review.')
+        .isLength({ min: 2})
+        .withMessage('Please add more details so we can post this review.')
+]
+
 router.get('/', asyncHandler(async (req, res) => {
     const reviews = await Review.findAll()
     res.json(reviews)
+}))
+
+router.post('/', asyncHandler(async (req, res) => {
+    const {
+        rating,
+        content,
+        userId,
+        businessId
+    } = req.body
+    const validatorErrors = validationResult(req)
+    if (validatorErrors.isEmpty()) {
+        const review = Review.build({
+            rating,
+            content,
+            userId,
+            businessId
+        })
+        await review.save()
+        res.json(review)
+    } else {
+        const errors = validatorErrors.array().map(err => err.msg);
+        res.json({
+            errors
+        })
+    }
 }))
 
 
