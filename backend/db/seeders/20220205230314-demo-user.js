701689664ca3f6d6f4ce1c339f93e76a1f35c302
Srdan <srdan.vorkapic@outlook.com>
2022-02-12 14:54:49 -0800

    Seeding done

--- backend/db/seeders/20220205230314-demo-user.js
+++ backend/db/seeders/20220205230314-demo-user.js
@@ -1,31 +1,103 @@
-'use strict';
-const bcrypt = require('bcryptjs');
+"use strict";
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
-      {
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password'),
-      },
-      {
-        email: 'a@a.com',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password1'),
-      },
-      {
-        email: 'b@b.com',
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password2'),
-      },
-    ], {});
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert(
+            "Users",
+            [
+                {
+                    email: "demo@user.io",
+                    username: "Demo-lition",
+                    hashedPassword: bcrypt.hashSync("password"),
+                },
+                {
+                    email: "owner@owner.com",
+                    username: "Owner",
+                    hashedPassword: bcrypt.hashSync("passwordOwner"),
+                },
+                {
+                    email: "1@1.com",
+                    username: "Mary",
+                    hashedPassword: bcrypt.hashSync("password1"),
+                },
+                {
+                    email: "2@2.com",
+                    username: "John",
+                    hashedPassword: bcrypt.hashSync("password2"),
+                },
+                {
+                    email: "3@3.com",
+                    username: "James",
+                    hashedPassword: bcrypt.hashSync("password3"),
+                },
+                {
+                    email: "4@4.com",
+                    username: "Emily",
+                    hashedPassword: bcrypt.hashSync("password4"),
+                },
+                {
+                    email: "5@5.com",
+                    username: "Robert",
+                    hashedPassword: bcrypt.hashSync("password5"),
+                },
+                {
+                    email: "6@6.com",
+                    username: "Anna",
+                    hashedPassword: bcrypt.hashSync("password6"),
+                },
+                {
+                    email: "7@7.com",
+                    username: "Michael",
+                    hashedPassword: bcrypt.hashSync("password7"),
+                },
+                {
+                    email: "8@8.com",
+                    username: "Susan",
+                    hashedPassword: bcrypt.hashSync("password8"),
+                },
+                {
+                    email: "9@9.com",
+                    username: "David",
+                    hashedPassword: bcrypt.hashSync("password9"),
+                },
+                {
+                    email: "10@10.com",
+                    username: "Lisa",
+                    hashedPassword: bcrypt.hashSync("password10"),
+                },
+                {
+                    email: "11@11.com",
+                    username: "Marc",
+                    hashedPassword: bcrypt.hashSync("password11"),
+                },
+            ],
+            {}
+        );
+    },
 
-  down: (queryInterface, Sequelize) => {
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
-  }
+    down: (queryInterface, Sequelize) => {
+        const Op = Sequelize.Op;
+        return queryInterface.bulkDelete(
+            "Users",
+            {
+                username: {
+                    [Op.in]: [
+                        "Demo-lition",
+                        "Mary",
+                        "John",
+                        "James",
+                        "Emily",
+                        "Robert",
+                        "Anna",
+                        "Michael",
+                        "Susan",
+                        "David",
+                        "Lisa",
+                    ],
+                },
+            },
+            {}
+        );
+    },
 };
