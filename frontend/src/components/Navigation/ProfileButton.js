12289b0c640e28e79c10369e30d33ac954e2e0b2
Srdan <srdan.vorkapic@outlook.com>
2022-02-12 17:04:32 -0800

    Navbar styling done

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -3,59 +3,63 @@ import { useDispatch } from "react-redux";
 import { NavLink, Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import "./ProfileButton.css";
+import logoutImage from '../../images/logout-button.png'
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
 
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = () => {
-      setShowMenu(false);
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
     };
 
-    document.addEventListener("click", closeMenu);
+    useEffect(() => {
+        if (!showMenu) return;
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
 
+        document.addEventListener("click", closeMenu);
 
-  const logout = e => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-    return <Redirect to='/' />
-  };
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
 
+    const logout = e => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+        return <Redirect to="/" />;
+    };
 
-  return (
-    <>
-
-      {/* <NavLink to='/notes' className="navbar-links">Notes</NavLink> */}
-      <button onClick={openMenu}>
-        <div id="icon-profile" >
-          <i className="fas fa-user-circle" />
-        </div>
-      </button>
-
-      {showMenu && (
-
-
-        <ul id="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-              <button onClick={logout} id="logout-button">Log Out</button>
-          </li>
-        </ul>
-      )}
-    </>
-  );
+    return (
+        <>
+            {/* <NavLink to='/notes' className="navbar-links">Notes</NavLink> */}
+            <div id="srki">
+                <div className="margin-right">
+                    <button onClick={openMenu} id="profile-icon-button">
+                        {/* <div id="profile-icon-image"> */}
+                            {/* <i className="fas fa-user-circle" /> */}
+                            <img src={logoutImage} id='profile-icon-image' />
+                        {/* </div> */}
+                    </button>
+                </div>
+                <div>
+                    {showMenu && (
+                        <div id="profile-dropdown">
+                            <div>{user.username}</div>
+                            <div>{user.email}</div>
+                            <div>
+                                <button onClick={logout} id="logout-button">
+                                    Log Out
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default ProfileButton;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,54 +3,70 @@ import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
-// import logo from "../../images/newLogo.png";
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector(state => state.session.user);
+    const sessionUser = useSelector(state => state.session.user);
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <>
-        <ProfileButton user={sessionUser} />
-        <NavLink to="/businesses" id="business-navlink">Businesses</NavLink>
-      </>
+    let sessionLinks;
+    if (sessionUser) {
+        sessionLinks = (
+            <>
+                <div className="margin-right">
+                    <NavLink to="/businesses" id="business-navlink">
+                        Businesses
+                    </NavLink>
+                </div>
+                <div>
+                    <ProfileButton user={sessionUser} className="margin-right"/>
+                </div>
+            </>
+        );
+    } else {
+        sessionLinks = (
+            <>
+                <div>
+                    <NavLink exact to="/" className="navbar-links margin-right">
+                        Home
+                    </NavLink>
+                </div>
+                <div>
+                    <NavLink to="/login" className="navbar-links margin-right">
+                        Log in
+                    </NavLink>
+                </div>
+                <div>
+                    <NavLink to="/signup" className="navbar-links margin-right">
+                        Sign up
+                    </NavLink>
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <div id="navbar">
+            {/* <div id="navbar-two"> */}
+            <div className="margin-right">
+                <a href="https://github.com/srvorkap" target="_blank">
+                    <span id="github-icon">
+                        <i class="fab fa-github"></i>
+                    </span>
+                </a>
+            </div>
+            <div className="margin-right">
+                <a
+                    href="https://www.linkedin.com/in/srdanvorkapic/"
+                    target="_blank"
+                >
+                    <span id="linkedin-icon">
+                        <i class="fab fa-linkedin"></i>
+                    </span>
+                </a>
+            </div>
+            <div id="navbar-conditional">{isLoaded && sessionLinks}</div>
+            {/* </div> */}
+        </div>
     );
-  } else {
-    sessionLinks = (
-      <>
-        <NavLink exact to="/" className="navbar-links">
-          Home
-        </NavLink>
-        <NavLink to="/login" className="navbar-links">
-          Log in
-        </NavLink>
-        <NavLink to="/signup" className="navbar-links">
-          Sign up
-        </NavLink>
-      </>
-    );
-  }
-
-  return (
-    <ul id="navbar">
-      <li id="navbar-two">
-        {/* <img src={logo} id="logo"></img> */}
-
-        <a href="https://github.com/srvorkap" target="_blank">
-          <span id="github-icon">
-            <i class="fab fa-github"></i>
-          </span>
-        </a>
-        <a href="https://www.linkedin.com/in/srdanvorkapic/" target="_blank">
-          <span id="linkedin-icon">
-            <i class="fab fa-linkedin"></i>
-          </span>
-        </a>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
-  );
 }
 
 export default Navigation;
