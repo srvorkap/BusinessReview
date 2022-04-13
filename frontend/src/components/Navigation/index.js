b4141f4952dc4f75c177259cd6872aa5be9ae21b
Srdan <srdan.vorkapic@outlook.com>
2022-04-12 23:16:16 -0700

    Positioning navbar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -7,65 +7,79 @@ import "./Navigation.css";
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
 
-    let sessionLinks;
-    if (sessionUser) {
-        sessionLinks = (
-            <>
-                <div className="margin-right">
-                    <NavLink to="/businesses" id="business-navlink">
-                        Businesses
-                    </NavLink>
-                </div>
-                <div>
-                    <ProfileButton
-                        user={sessionUser}
-                        className="margin-right"
-                    />
-                </div>
-            </>
-        );
-    } else {
-        sessionLinks = (
-            <>
-                {/* <div>
-                    <NavLink exact to="/" className="links margin-right">
-                        Home
-                    </NavLink>
-                </div> */}
-                <div>
-                    <NavLink to="/login" className="links margin-right">
-                        Log In
-                    </NavLink>
-                </div>
-                <div>
-                    <NavLink to="/signup" className="links margin-right">
-                        Sign Up
-                    </NavLink>
-                </div>
-            </>
-        );
-    }
-
     return (
         <div id="navbar">
-            <div className="margin-right">
-                <a href="https://github.com/srvorkap" target="_blank">
-                    <span id="github-icon">
-                        <i class="fab fa-github"></i>
-                    </span>
-                </a>
+
+
+            <div id="navbar-center">
+                <div className="margin-right">
+                    <a href="https://srvorkap.github.io/" target="_blank">
+                        <span id="github-icon">
+                            <div>Portfolio</div>
+                        </span>
+                    </a>
+                </div>
+                <div className="margin-right">
+                    <a href="https://github.com/srvorkap" target="_blank">
+                        <span id="github-icon">
+                            <i class="fab fa-github"></i>
+                        </span>
+                    </a>
+                </div>
+                <div className="margin-right">
+                    <a
+                        href="https://www.linkedin.com/in/srdanvorkapic/"
+                        target="_blank"
+                    >
+                        <span id="linkedin-icon">
+                            <i class="fab fa-linkedin"></i>
+                        </span>
+                    </a>
+                </div>
+                <div className="margin-right">
+                    <a href="https://angel.co/u/srdan-vorkapic" target="_blank">
+                        <span id="linkedin-icon">
+                            <i class="fab fa-angellist"></i>
+                        </span>
+                    </a>
+                </div>
+                {sessionUser && (
+                    <div className="margin-right">
+                        <NavLink to="/businesses" id="business-navlink">
+                            Businesses
+                        </NavLink>
+                    </div>
+                )}
             </div>
-            <div className="margin-right">
-                <a
-                    href="https://www.linkedin.com/in/srdanvorkapic/"
-                    target="_blank"
-                >
-                    <span id="linkedin-icon">
-                        <i class="fab fa-linkedin"></i>
-                    </span>
-                </a>
+
+            <div id="navbar-right">
+                {sessionUser ? (
+                    <div>
+                        <ProfileButton
+                            user={sessionUser}
+                            className="margin-right"
+                        />
+                    </div>
+                ) : (
+                    <>
+                        <div>
+                            <NavLink to="/login" className="links margin-right">
+                                Log In
+                            </NavLink>
+                        </div>
+                        <div>
+                            <NavLink
+                                to="/signup"
+                                className="links margin-right"
+                            >
+                                Sign Up
+                            </NavLink>
+                        </div>
+                    </>
+                )}
             </div>
-            <div id="navbar-conditional">{isLoaded && sessionLinks}</div>
+
+
         </div>
     );
 }
